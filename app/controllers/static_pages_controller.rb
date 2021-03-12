9ced7bb5f884a1ad58fc2cdcecba41b9f3dddcf4
WarlockHolmes <grau.morgan@gmail.com>
2021-03-12 10:28:08 -0700

    Property front-end
    Added property component and styles, updated routes, updated Static pages controller, added missing jbuilder file for showing properties

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -2,4 +2,8 @@ class StaticPagesController < ApplicationController
   def home
     render 'home'
   end
+  def property
+    @data = { property_id: params[:id] }.to_json
+    render 'property'
+  end
 end
