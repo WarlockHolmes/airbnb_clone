353046190dc26be842cc3853b4b5df3b580de6b9
WarlockHolmes <grau.morgan@gmail.com>
2021-03-13 11:39:33 -0700

    Payment Frontend
    Added Stripe JS library, configured webpack environment file, updated booking component with payment method, set up webhooks.

--- app/javascript/src/property/bookingWidget.jsx
+++ app/javascript/src/property/bookingWidget.jsx
@@ -39,28 +39,54 @@ class BookingWidget extends React.Component {
   }
 
   submitBooking = (e) => {
-    if (e) { e.preventDefault(); }
-    const { startDate, endDate } = this.state;
-    console.log(startDate.format('MMM DD YYYY'), endDate.format('MMM DD YYYY'));
+   if (e) { e.preventDefault(); }
+   const { startDate, endDate } = this.state;
+   console.log(startDate.format('MMM DD YYYY'), endDate.format('MMM DD YYYY'));
 
-    fetch(`/api/bookings`, safeCredentials({
-      method: 'POST',
-        body: JSON.stringify({
-          booking: {
-            property_id: this.props.property_id,
-            start_date: startDate.format('MMM DD YYYY'),
-            end_date: endDate.format('MMM DD YYYY'),
-          }
-        })
-    }))
-      .then(handleErrors)
-      .then(response => {
-        console.log(response);
-      })
-      .catch(error => {
-        console.log(error);
-      })
-  }
+   fetch(`/api/bookings`, safeCredentials({
+     method: 'POST',
+       body: JSON.stringify({
+         booking: {
+           property_id: this.props.property_id,
+           start_date: startDate.format('MMM DD YYYY'),
+           end_date: endDate.format('MMM DD YYYY'),
+         }
+       })
+   }))
+     .then(handleErrors)
+     .then(response => {
+       return this.initiateStripeCheckout(response.booking.id)
+     })
+     .catch(error => {
+       console.log(error);
+     })
+ }
+
+ initiateStripeCheckout = (booking_id) => {
+   return fetch(`/api/charges?booking_id=${booking_id}&cancel_url=${window.location.pathname}`, safeCredentials({
+     method: 'POST',
+   }))
+     .then(handleErrors)
+     .then(response => {
+       const stripe = Stripe(process.env.STRIPE_PUBLISHABLE_KEY);
+
+       stripe.redirectToCheckout({
+         // Make the id field from the Checkout Session creation API response
+         // available to this file, so you can provide it as parameter here
+         // instead of the {{CHECKOUT_SESSION_ID}} placeholder.
+         sessionId: response.charge.checkout_session_id,
+       }).then((result) => {
+         // If `redirectToCheckout` fails due to a browser or network
+         // error, display the localized error message to your customer
+         // using `result.error.message`.
+
+       });
+     })
+     .catch(error => {
+       console.log(error);
+     })
+ }
+
 
   onDatesChange = ({ startDate, endDate }) => this.setState({ startDate, endDate })
 
